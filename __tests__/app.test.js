31c3530b869f9e81e246b18df0b49574eb0f0666
mikael-v <mikael-v@outlook.com>
2024-05-28 16:28:46 +0100

    completes task 4 get/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -22,3 +22,40 @@ describe('/api/topics', () => {
             })
     });
 });
+
+describe('/api/articles/:article_id', () => {
+    test('should respond with a 200 status code and on object containing the correct properties', () => {
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response) => {
+            const article = response.body.article
+                expect(typeof article.article_id).toBe('number');
+                expect(typeof article.title).toBe('string');
+                expect(typeof article.topic).toBe('string');
+                expect(typeof article.author).toBe('string');
+                expect(typeof article.body).toBe('string');
+                expect(typeof article.created_at).toBe('string');
+                expect(typeof article.votes).toBe('number');
+                expect(typeof article.article_img_url).toBe('string');
+        })
+        
+    });
+    test('should return a 404 status code and appropriate message if given a valid but non-existent id number', () => {
+        return request(app)
+        .get('/api/articles/999')
+        .expect(404)
+        .then((response)=>{
+            expect(response.body.msg).toBe('article at given id does not exist')
+        })
+    });
+    test('should return a 400 status code if given an invalid id', () => {
+        //error if an invalid id given, if it isn't a number
+        return request(app)
+        .get('/api/articles/banana')
+        .expect(400)
+        .then((response)=>{
+            expect(response.body.msg).toBe('Bad request')
+        })
+    });
+});
--- db/app.js
+++ db/app.js
@@ -2,7 +2,25 @@ const express = require('express')
 const app = express();
 
 const { getTopics } = require('../controllers/topics.controllers')
+const {getArticleById } = require('../controllers/articles.controllers')
 
 app.get('/api/topics', getTopics)
+app.get('/api/articles/:article_id', getArticleById)
+
+app.use((err, req, res, next)=>{
+    if(err.code){
+      res.status(400).send({msg: 'Bad request'})
+    } else{
+      next(err)
+    }
+  })
+  
+  app.use((err, req, res, next) =>{
+    if(err.msg){
+      res.status(err.status).send({msg: err.msg})
+    } else{
+      next(err)
+    }
+  })
 
 module.exports = app;
